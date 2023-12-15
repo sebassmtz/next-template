55baa4f6cd7d6db3b6bbfe8ab9651a5912aa75dc
sebastian <sebastian.martinez07@uptc.edu.co>
2023-12-15 18:13:10 -0500

    Changed messages

--- src/app/[locale]/path/page.tsx
+++ src/app/[locale]/path/page.tsx
@@ -1,20 +1,25 @@
-import {useTranslations} from 'next-intl';
-import {unstable_setRequestLocale} from 'next-intl/server';
+import PageLayout from "@/components/PageLayout/PageLayout";
+import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 type Props = {
-  params: {locale: string};
+  params: { locale: string };
 };
 
-export default function PathnamesPage({params: {locale}}: Props) {
+export default function PathnamesPage({ params: { locale } }: Props) {
   // Enable static rendering
   unstable_setRequestLocale(locale);
 
-  const t = useTranslations('PathnamesPage');
+  const t = useTranslations("PathnamesPage");
 
   return (
-    <div>
-      <h1>{t('title')}</h1>
-      <p>{t('description')}</p>
-    </div>
+    <PageLayout>
+      <div>
+        {t.rich("description", {
+          p: (chunks) => <p>{chunks}</p>,
+          code: (chunks) => <code>{chunks}</code>,
+        })}
+      </div>
+    </PageLayout>
   );
 }
